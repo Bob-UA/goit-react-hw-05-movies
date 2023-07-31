e10243c171ee05b06b7aeae1a232705d8e68476d
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-07-30 21:23:16 -0700

    completed

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
+import Layout from './Layout';
+const Home = lazy(() => import('./Home'));
+const NotFound = lazy(() => import('./NotFound'));
+const Movies = lazy(() => import('./Movies'));
+const Cast = lazy(() => import('./Cast'));
+const MovieDetails = lazy(() => import('./MovieDetails'));
+const Reviews = lazy(() => import('./Reviews'));
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+};
